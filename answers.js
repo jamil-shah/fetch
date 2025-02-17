a9b1df70146f484e655b2d9d1b83895bfcb1e2ff
jamil-shah <73444183+jamil-shah@users.noreply.github.com>
2025-02-17 01:34:24 +0000

    step 2

--- answers.js
+++ answers.js
@@ -1,6 +1,7 @@
 fetch('https://msu-webdev.github.io/api/start.json')
     .then(response => response.json())
-    .then(json => console.log(json));
+    .then(json => {
+        console.log('Step 1 data:',json);
 
     const earthtomoon = 238900;
     const moondiameter = 2159;
@@ -9,4 +10,26 @@ fetch('https://msu-webdev.github.io/api/start.json')
 
     console.log('Answer:',moons);
 
-    
+    const nexturl = 'https://msu-webdev.github.io/api/110.json';
+    return fetch(nexturl);
+    })
+
+    .then(response => response.json())
+    .then(data => {
+        console.log('Step 2 data:',data);
+
+        const enroll2023 = 51289;
+        const enroll2024 = 52089;
+
+        const difference = enroll2024 - enroll2023;
+        const percentChange = (difference / enroll2023) * 100;
+
+        const step2Answer = Math.round(percentChange);
+
+        console.log('Answer to Step 2:', step2Answer);
+
+        const nexturl2 = 'https://msu-webdev.github.io/api/2.json'
+        return fetch(nexturl2)
+})
+
+ 
