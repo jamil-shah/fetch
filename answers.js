4e6ce0b079f125983c580de225033bf710625c65
jamil-shah <73444183+jamil-shah@users.noreply.github.com>
2025-02-17 00:04:18 +0000

    math riddle part 1

--- answers.js
+++ answers.js
@@ -1,1 +1,12 @@
+fetch('https://msu-webdev.github.io/api/start.json')
+    .then(response => response.json())
+    .then(json => console.log(json));
 
+    const earthtomoon = 238900;
+    const moondiameter = 2159;
+
+    const moons = Math.floor(earthtomoon/moondiameter);
+
+    console.log('Answer:',moons);
+
+    
